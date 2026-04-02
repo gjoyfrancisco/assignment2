7686f4fbdc147993ae24a15d65964ff72f984860
gjoyfrancisco <fr.grecille@gmail.com>
2026-04-01 20:28:31 -0600

    component and update test case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Joy Franciscogit </h1>
+      <h1>Joy Francisco</h1>
     </div>
   );
 }
