f046c6b43ca52fa6d7b822f8dbf30c941965db85
gjoyfrancisco <fr.grecille@gmail.com>
2026-04-01 21:49:00 -0600

    Add watermelon image and display it in the App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import watermelon from './watermelon.png';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <h1>Joy Francisco</h1>
       <h2>TECH2102 Enterprise Computing</h2>
       <h3>Assignment 2</h3>
+      <img src={watermelon} alt="watermelon" />
     </div>
   );
 }
